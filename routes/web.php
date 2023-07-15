8d9068101f2ef9d1d0ae327d40a3673eec929cfd
MSyaifulIslam-lab <syaifulislam4013@gmail.com>
2023-07-16 00:14:39 +0700

    Fixed login and logout

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\car_controller;
 use App\Http\Controllers\dashboard_controller;
 use App\Http\Controllers\BookingController;
@@ -27,58 +28,62 @@ Route::get('/', function(){
 });
 
 // Nampilkan halaman user
-Route::get('/user', [user_controller::class, 'index']);
+Route::get('/user', [user_controller::class, 'index'])->middleware('auth');
 
 //Nampillin form tambah user
-Route::get('/user/create', [user_controller::class, 'create']);
+Route::get('/user/create', [user_controller::class, 'create'])->middleware('auth');
 
 //Nampillin tambah user
-Route::post('/user', [user_controller::class, 'store']);
+Route::post('/user', [user_controller::class, 'store'])->middleware('auth');
 
 //Nampillin form edit user
-Route::get('/user/edit/{id}', [user_controller::class, 'edit']);
+Route::get('/user/edit/{id}', [user_controller::class, 'edit'])->middleware('auth');
 
 //Nampillin edit user
-Route::put('/user/{id}', [user_controller::class, 'update']);
+Route::put('/user/{id}', [user_controller::class, 'update'])->middleware('auth');
 
 //Nampillin delete user
-Route::delete('/user', [user_controller::class, 'destroy']);
+Route::delete('/user', [user_controller::class, 'destroy'])->middleware('auth');
 
 // nampilin halaman mobil
-Route::get('/car', [car_controller::class, 'index']);
+Route::get('/car', [car_controller::class, 'index'])->middleware('auth');
 
 // nampilin form tambah mobil
-Route::get('/car/create', [car_controller::class, 'create']);
+Route::get('/car/create', [car_controller::class, 'create'])->middleware('auth');
 
 // proses tambah mobil
-Route::post('/car', [car_controller::class, 'store']);
+Route::post('/car', [car_controller::class, 'store'])->middleware('auth');
 
 // nampilin form edit mobil
-Route::get('/car/edit/{id}', [car_controller::class, 'edit']);
+Route::get('/car/edit/{id}', [car_controller::class, 'edit'])->middleware('auth');
 
 // proses edit mobil
-Route::put('/car/{id}', [car_controller::class, 'update']);
+Route::put('/car/{id}', [car_controller::class, 'update'])->middleware('auth');
 
 // delete mobil
-Route::delete('/car', [car_controller::class, 'destroy']);
+Route::delete('/car', [car_controller::class, 'destroy'])->middleware('auth');
 
 // Menampilkan Daftar Booking
-Route::get('/bookings', [BookingController::class, 'index']);
+Route::get('/bookings', [BookingController::class, 'index'])->middleware('auth');
 
 // Menampilkan Form Tambah Booking
-Route::get('/bookings/create', [BookingController::class, 'create']);
+Route::get('/bookings/create', [BookingController::class, 'create'])->middleware('auth');
 
 // Proses Tambah Booking
-Route::post('bookings', [BookingController::class, 'store']);
+Route::post('bookings', [BookingController::class, 'store'])->middleware('auth');
 
 // Menampilkan Form Edit Booking
-Route::get('/bookings/edit/{id}', [BookingController::class, 'edit']);
+Route::get('/bookings/edit/{id}', [BookingController::class, 'edit'])->middleware('auth');
 
 // Proses Edit Booking
-Route::put('/bookings/{id}', [BookingController::class, 'update']);
+Route::put('/bookings/{id}', [BookingController::class, 'update'])->middleware('auth');
 
 // Route untuk delete Booking
-Route::delete('/bookings', [BookingController::class, 'destroy']);
+Route::delete('/bookings', [BookingController::class, 'destroy'])->middleware('auth');
 
 // Route Autentikasi
 Auth::routes();
+
+// route logout
+Route::post('/logout', [LoginController::class, 'logout'])->name('logout')->middleware('auth');
+Auth::logout();
